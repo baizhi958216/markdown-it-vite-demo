4ffd88ce790d441128fda90819f48a59dab448cf
baizhi958216 <1475289190@qq.com>
2024-07-06 12:44:32 +0800

    feat 字体呼啸呼啸渐变
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
   <div class="markdown-body" v-html="html"></div>
+  <div class="hover-underline-animation" ref="hoverRef">
+    hover-underline-animationhover-underline-animationh
+  </div>
 </template>
 
 <script lang="ts" setup>
 import MarkdownIt from "markdown-it";
 import Hello from "./md/Hello.md?raw";
+import { onMounted, onUnmounted, ref } from "vue";
+
 const md = new MarkdownIt({});
+
 const html = md.render(Hello);
+
+const hoverRef = ref<HTMLElement | null>(null);
+
+const changeWidth = (newWidth: string) => {
+  if (hoverRef.value) {
+    hoverRef.value.style.setProperty("--underline-width", newWidth);
+  }
+};
+
+onMounted(() => {
+  hoverRef.value?.addEventListener("mousemove", (e) => {
+    const { left } = hoverRef.value!.getBoundingClientRect();
+    const mouseX = e.clientX - left;
+    changeWidth(mouseX + "px");
+  });
+});
+
+onUnmounted(() => {
+  hoverRef.value?.removeEventListener("mousemove", () => {});
+});
 </script>
 
 <style lang="scss">
@@ -43,5 +69,45 @@ const html = md.render(Hello);
       color: #f7b731;
     }
   }
+}
+
+@keyframes slide {
+  from {
+    background-position: 0% 0;
+  }
+  to {
+    background-position: 100% 0;
+  }
+}
+
+.hover-underline-animation {
+  display: inline-block;
+  position: relative;
+  cursor: default; /* 鼠标遇到字体会变成I, 给默认好看点 */
+}
+
+.hover-underline-animation::after {
+  content: "";
+  position: absolute;
+  width: var(--underline-width, 12px);
+  height: 2px; /* 线条高度 */
+  bottom: 0;
+  left: 0;
+  background-image: linear-gradient(
+    90deg,
+    #778beb,
+    #e15f41,
+    #3dc1d3
+  ); /* 呼啸呼啸渐变 */
+  background-size: 200% 100%; /* 背景大小 */
+  background-position: -100% 0; /* 初始背景位置 */
+  opacity: 0;
+  transition: opacity 0.3s, background-position 0.3s;
+}
+
+.hover-underline-animation:hover::after {
+  background-position: 0 0;
+  opacity: 1;
+  animation: slide 3s linear infinite;
 }
 </style>
