78da9f217ef0e655a9d725740299f95c439a5ad6
baizhi958216 <1475289190@qq.com>
2024-07-06 11:53:23 +0800

    feat: Markdown-it示例
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <div></div>
+  <div v-html="html"></div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import MarkdownIt from "markdown-it";
+
+const md = new MarkdownIt();
+
+const html = md.render("# Hello, World");
+</script>
 
 <style lang="scss"></style>
