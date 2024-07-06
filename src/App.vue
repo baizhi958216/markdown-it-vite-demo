4f788e50d51e8084a8207f097772ab48268837e5
baizhi958216 <1475289190@qq.com>
2024-07-06 12:44:09 +0800

    feat: markdown样式
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
 <template>
-  <div v-html="html"></div>
+  <div class="markdown-body" v-html="html"></div>
 </template>
 
 <script lang="ts" setup>
 import MarkdownIt from "markdown-it";
-
-const md = new MarkdownIt();
-
-const html = md.render("# Hello, World");
+import Hello from "./md/Hello.md?raw";
+const md = new MarkdownIt({});
+const html = md.render(Hello);
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.markdown-body {
+  h1 {
+    color: #fc5c65;
+  }
+
+  h2 {
+    color: #fd9644;
+  }
+
+  h3 {
+    color: #26de81;
+  }
+
+  p {
+    color: #2bcbba;
+  }
+
+  blockquote p {
+    color: #546de5;
+  }
+
+  ul li {
+    color: #a55eea;
+  }
+
+  pre {
+    code.language-js {
+      color: #45aaf2;
+    }
+    code.language-java {
+      color: #f7b731;
+    }
+  }
+}
+</style>
---
+++ src/md/Hello.md
@@ -0,0 +1,22 @@
+# Hello World
+
+## Archlinux yyds
+
+### Not use Ubuntu
+
+> v me 50, pls
+
+btw, grub is useful
+
+- and the best code editor is vscode
+
+```js
+const num1 = 1;
+const num2 = 2;
+```
+
+```java
+public class Object extends Yushengjun implements Mashibing {
+    String md = "This is Markdown";
+}
+```
